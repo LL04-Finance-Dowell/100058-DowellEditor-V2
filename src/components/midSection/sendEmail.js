37238badacbc9b90ce3e50df6ce14c639090bfd3
John Patrick <jpattrick538@gmail.com>
2023-10-23 13:12:58 +0300

    feat:add loading animation and close sidebar on successful sent email

--- src/components/midSection/sendEmail.js
+++ src/components/midSection/sendEmail.js
@@ -8,25 +8,25 @@ const animateDots = (loadingDotsElement, dotCount, cancel = false) => {
         clearInterval(interval);
         loadingDotsElement.innerHTML = 'Configure Mail';
     } else {
-        loadingDotsElement.innerHTML = '';
+        loadingDotsElement.innerHTML = 'sending';
         let count = 0;
         interval = setInterval(() => {
             loadingDotsElement.innerHTML += '.';
             count++;
             if (count === dotCount) {
                 count = 0;
-                loadingDotsElement.innerHTML = '';
+                loadingDotsElement.innerHTML = 'sending';
             }
         }, 300);
     }
 }
-export const sendEmail = (formData, buttonField) => {
+export const sendEmail = (formData, buttonField,setSideBar) => {
     let data = formData;
 
     let config = {
         method: 'post',
         maxBodyLength: Infinity,
-        url: 'https://100085.pythonanywhere.com/api/v1/mail/c30e4e5f-52fd-4658-8de8-d682e8e4d2ae/?type=send-email',
+        url: 'https://100085.pythonanywhere.com/api/uxlivinglab/email/',
         data: data
     };
     animateDots(buttonField, 4)
@@ -34,8 +34,8 @@ export const sendEmail = (formData, buttonField) => {
         .then((response) => {
             console.log(JSON.stringify(response.data));
             animateDots(buttonField, 6, true)
-
-            toast.success('email sent successfully')
+            toast.success('email sent successfully');
+            setSideBar(false)
         })
         .catch((error) => {
             console.log(error);
